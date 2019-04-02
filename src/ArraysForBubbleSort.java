159a0626aad3b0f7203eb6c96f95e764e206d2bf
Spawn <alkspawn@gmail.com>
2019-04-02 17:48:11 +0300

    final

--- src/ArraysForBubbleSort.java
+++ src/ArraysForBubbleSort.java
@@ -2,45 +2,65 @@ import java.util.Scanner;
 
 public class ArraysForBubbleSort {
 
-    public static int toPrint() {
-
+    public static String readUserInput() {
         Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine();
-        try {
-
-            int xxx = Integer.parseInt(string);
-            if (xxx > 0 && xxx < 1000000) {
-                return xxx;
-            } else
-                System.out.println("Sorry. Restart the program and try again!");
-            System.exit(0);
-        } catch (NumberFormatException e) {
-            System.out.println("Sorry. Restart the program and try again!");
-            System.exit(0);
-        }
-        return 0;
-    }
-
-    public static double toArrayNumber(int i) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Assign number to index " + i);
-        boolean count = true;
-        while (count) {
-            String strin = scan.nextLine();
-            try {
-                double xxx = Double.parseDouble(strin);
-                if (xxx < Integer.MAX_VALUE) {
-                    count = false;
-                    return xxx;
-                } else
-                    System.out.println("Sorry. Try again!");
-            } catch (NumberFormatException e) {
-                System.out.println("Sorry.Try again!");
-
+        String arrayNumber = "";
+        while (scanner.hasNextLine()) {
+            boolean valid = true;
+            arrayNumber = scanner.nextLine();
+            String checkString = arrayNumber.replaceAll("[\\s-.]", "");
+            for (int i = 0; i < checkString.length(); i++) {
+                char digit = checkString.charAt(i);
+                if (Character.isDigit(digit)) {
+                    continue;
+                } else {
+                    System.out.println("Sorry. Try again");
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                break;
             }
         }
-        scan.close();
-        return 0;
+        return arrayNumber;
+    }
+
+    public static int countArrayLength(String numberArray) {
+        numberArray = numberArray.trim();
+        numberArray = numberArray.replaceAll("[-.]", "");
+        int count = 1;
+        for (int i = 0; i < numberArray.length(); i++) {
+            char digit = numberArray.charAt(i);
+            if (Character.isSpaceChar(digit)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static double[] addArray(int count, String arrayNumber) throws Exception{
+        StringBuilder builder = new StringBuilder();
+        double[] arr = new double[count];
+        String stringBuilder;
+        double digitDouble;
+        int j = 0;
+        for (int i = 0; i < arrayNumber.length(); i++) {
+            char digitChar = arrayNumber.charAt(i);
+            if (Character.isSpaceChar(digitChar)) {
+                stringBuilder = builder.toString();
+
+                digitDouble = Double.parseDouble(stringBuilder);
+                arr[j] = digitDouble;
+                builder.setLength(0);
+                j++;
+            }
+            builder.append(digitChar);
+        }
+        stringBuilder = builder.toString();
+        digitDouble = Double.parseDouble(stringBuilder);
+        arr[j] = digitDouble;
+        return arr;
     }
 
     public static void printArray(double[] arr) {
@@ -52,29 +72,35 @@ public class ArraysForBubbleSort {
     }
 
 
-    private static void bubbleSort(double[] arr) {
+    public static void bubbleSort(double[] arr) {
         for (int i = arr.length - 1; i > 0; i--) {
+            boolean valid = true;
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    valid = false;
                     double tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
                 }
             }
+            if (valid) {
+                break;
+            }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("This program prints an array of dimension N, filled with numbers.");
-        System.out.println("write number from 1 before 1000000: ");
-        int n = toPrint();
-        double[] arr = new double[n];
-        for (int i = 0; i < arr.length; i++) {
-            double xxx = toArrayNumber(i);
-            arr[i] = xxx;
+        System.out.println("Enter your array : ");
+        String arrayNumber = readUserInput();
+        int count = countArrayLength(arrayNumber);
+        try {
+          double []  arr = addArray(count, arrayNumber);
+            bubbleSort(arr);
+            printArray(arr);
+        } catch (Exception e){
+            System.out.println("Sorry. Try again.");
         }
-        bubbleSort(arr);
-        printArray(arr);
-    }
 
+    }
 }
